c1b94c14469a2c25e7333165686895203f9aadfd
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2021-12-23 12:36:36 +0000

    Add onclick javascript function

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,5 @@
-function new_text() {
-    var text = "...you are not listening!"
-document.getElementById("paragraph").innerText = text
+function new_text() { //define the function new_text()
+    var text = "...you are not listening!" //create and assign a string to variable text
+    text += " Aren't you?" // concatenate a new string to the string already assigned to variable text 
+document.getElementById("paragraph").innerText = text // change the html iner text with the new text
 }
