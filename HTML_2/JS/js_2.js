fd95814a0c2b9e379950f58d4c40a99390db5c8b
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-02-10 21:06:41 +0000

    HTML_2
    this is a form

---
+++ HTML_2/JS/js_2.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    var fname = document.forms["myForm"]["firstname"].value;
+    var lname = document.forms["myForm"]["lastname"].value;
+    var pet = document.forms["myForm"]["fav_pet"].value;
+    if (fname == "") {
+        alert("First Name must be filled out");
+        return false;
+    } else if (lname == "") {
+        alert("Last Name must be filled out");
+        return false;
+    } else if (pet == "") {
+        alert("Please chose your favourite pet");
+        return false;
+    }
+    }
+}
